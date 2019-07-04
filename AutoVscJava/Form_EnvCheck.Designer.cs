9838c6a0f1a264cfd83a30af4eefb93158a9d823
SDchao <1511047037@qq.com>
2019-07-04 10:56:17 +0800

    完成主界面检查功能

--- AutoVscJava/Form_EnvCheck.Designer.cs
+++ AutoVscJava/Form_EnvCheck.Designer.cs
@@ -30,7 +30,11 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_EnvCheck));
             this.Label_jdk = new System.Windows.Forms.Label();
-            this.label_jdk_status = new System.Windows.Forms.Label();
+            this.Label_jdk_status = new System.Windows.Forms.Label();
+            this.Button_install = new System.Windows.Forms.Button();
+            this.Button_next = new System.Windows.Forms.Button();
+            this.PictureBox_status = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox_status)).BeginInit();
             this.SuspendLayout();
             // 
             // Label_jdk
@@ -38,22 +42,46 @@
             resources.ApplyResources(this.Label_jdk, "Label_jdk");
             this.Label_jdk.Name = "Label_jdk";
             // 
-            // label_jdk_status
+            // Label_jdk_status
             // 
-            resources.ApplyResources(this.label_jdk_status, "label_jdk_status");
-            this.label_jdk_status.Name = "label_jdk_status";
+            resources.ApplyResources(this.Label_jdk_status, "Label_jdk_status");
+            this.Label_jdk_status.Name = "Label_jdk_status";
+            // 
+            // Button_install
+            // 
+            resources.ApplyResources(this.Button_install, "Button_install");
+            this.Button_install.Name = "Button_install";
+            this.Button_install.UseVisualStyleBackColor = true;
+            // 
+            // Button_next
+            // 
+            resources.ApplyResources(this.Button_next, "Button_next");
+            this.Button_next.Name = "Button_next";
+            this.Button_next.UseVisualStyleBackColor = true;
+            // 
+            // PictureBox_status
+            // 
+            this.PictureBox_status.Image = global::AutoVscJava.Properties.Resources.loading_small;
+            resources.ApplyResources(this.PictureBox_status, "PictureBox_status");
+            this.PictureBox_status.Name = "PictureBox_status";
+            this.PictureBox_status.TabStop = false;
             // 
             // Form_EnvCheck
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.label_jdk_status);
+            this.Controls.Add(this.Button_next);
+            this.Controls.Add(this.Button_install);
+            this.Controls.Add(this.PictureBox_status);
+            this.Controls.Add(this.Label_jdk_status);
             this.Controls.Add(this.Label_jdk);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form_EnvCheck";
             this.ShowIcon = false;
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form_EnvCheck_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox_status)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -61,7 +89,10 @@
 
         #endregion
         private System.Windows.Forms.Label Label_jdk;
-        private System.Windows.Forms.Label label_jdk_status;
+        private System.Windows.Forms.Label Label_jdk_status;
+        private System.Windows.Forms.PictureBox PictureBox_status;
+        private System.Windows.Forms.Button Button_install;
+        private System.Windows.Forms.Button Button_next;
     }
 }
 
--- AutoVscJava/Form_EnvCheck.cs
+++ AutoVscJava/Form_EnvCheck.cs
@@ -6,8 +6,10 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 using AutoVscJava.Classes;
+using AutoVscJava.Properties;
 
 namespace AutoVscJava
 {
@@ -20,7 +22,31 @@ namespace AutoVscJava
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+            CheckForIllegalCrossThreadCalls = false;
+
+            Thread checkJdkThread = new Thread(() =>
+            {
+                if(!EnvChecker.CheckJavaSE())
+                {
+                    Label_jdk_status.Text = "已安装";
+                    Label_jdk_status.ForeColor = Color.Green;
+                    PictureBox_status.Image = Resources.complete;
+                    Button_next.Enabled = true;
+                } else
+                {
+                    Label_jdk_status.Text = "未安装";
+                    Label_jdk_status.ForeColor = Color.Red;
+                    PictureBox_status.Image = Resources.error;
+                    Button_install.Show();
+                }
+            });
+
+            checkJdkThread.Start();
+        }
+
+        private void Form_EnvCheck_Shown(object sender, EventArgs e)
+        {
+            Button_install.Hide();
         }
     }
 }
